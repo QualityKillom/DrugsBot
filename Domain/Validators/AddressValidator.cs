3fcf759d68484739d887b8a8500d0618e07e304a
HOME-PC\QualityKillom <qualitykillomwork@gmail.com>
2024-11-12 01:05:08 +0300

    +AddressValidator

---
+++ Domain/Validators/AddressValidator.cs
@@ -0,0 +1,33 @@
+﻿
+using Domain.ValueObjects;
+using FluentValidation;
+
+namespace Domain.Validators;
+
+public class AddressValidator: AbstractValidator<Address>
+{
+    public AddressValidator()
+    {
+        RuleFor(a => a.Street)
+            .NotNull().WithMessage(ValidationMessage.NotNull)
+            .NotEmpty().WithMessage(ValidationMessage.NotEmpty)
+            .Length(3, 100).WithMessage(ValidationMessage.WrongLenght);
+        
+        RuleFor(a=>a.City)
+            .NotNull().WithMessage(ValidationMessage.NotNull)
+            .NotEmpty().WithMessage(ValidationMessage.NotEmpty)
+            .Length(5, 50).WithMessage(ValidationMessage.WrongLenght);
+        
+        RuleFor(a=>a.House)
+            .NotNull().WithMessage(ValidationMessage.NotNull)
+            .NotEmpty().WithMessage(ValidationMessage.NotEmpty)
+            .GreaterThan(0).WithMessage(ValidationMessage.WrongPolarity);
+
+        RuleFor(a => a.PostalCode)
+            .NotNull().WithMessage(ValidationMessage.NotNull)
+            .NotEmpty().WithMessage(ValidationMessage.NotEmpty)
+            .GreaterThanOrEqualTo(10000).WithMessage(ValidationMessage.WrongNumber)
+            .LessThanOrEqualTo(999999).WithMessage(ValidationMessage.WrongNumber);
+        
+    }
+}
